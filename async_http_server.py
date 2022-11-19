29f8ad6eed90b78ee324a61c87489363b4c8263b
decimalpack <decimalpack@users.noreply.github.com>
2022-11-18 19:25:25 -0500

    Add beacon

--- async_http_server.py
+++ async_http_server.py
@@ -1,28 +1,32 @@
 from aiohttp import web
 from aiohttp import ClientSession
 from functools import cache
+
 app = web.Application()
 routes = web.RouteTableDef()
 cache = {}
 
 
 async def fetch_from_origin(path):
-    if path in cache: 
+    if path in cache:
         return cache[path]
-    repsonse = None
     async with ClientSession() as session:
         async with session.get(
                 f"http://cs5700cdnorigin.ccs.neu.edu:8080/{path}") as resp:
-            response =  await resp.text()
-    cache[path] = response
-    return response
+            cache[path] = await resp.text()
+            return cache[path]
+
+
+@routes.get("/grading/beacon")
+async def beacon(request):
+    return web.Response(text="", status=204)
 
 
 @routes.get("/{path:.*}")
-async def respond(request):
+async def proxy(request):
     resp = await fetch_from_origin(request.match_info["path"])
     return web.Response(text=resp, content_type="text/html")
 
 
 app.add_routes(routes)
-web.run_app(app)
+web.run_app(app, port=25015)
