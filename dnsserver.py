232d4075ea6e3f7b7e58ba7770ced39b379ed3e4
Gowtham Potnuru <gowtham7991@LAPTOP-5N1QNK9T.localdomain>
2022-11-28 17:10:11 -0500

    updated

--- dnsserver.py
+++ dnsserver.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
 """Strategy: Always origin"""
 import argparse
 import logging
 import socket
 from typing import Tuple
 import threading
-import heapq
 from queue import PriorityQueue
 from dnslib import *
 import requests
@@ -29,15 +27,17 @@ def get_ip_address():
 ORIGIN = "cs5700cdnorigin.ccs.neu.edu"
 ORIGIN_IP = socket.gethostbyname(ORIGIN)
 CLIENT_REPLICA_ROUTING_TABLE = {}
-REPLICA_SERVERS = []
+REPLICA_SERVERS = {'139.144.30.25' : 'proj4-repl1.5700.network'}
+UPDATE_ROUTING_TABLE_FREQUENCY = 300
+PING_REPLICA_SERVERS_FREQUENCY = 100
 
 def find_closest_replica_server(source_ip):
     source_coordinates = find_location_coordinates(source_ip)
-    replica_servers = ['175.253.115.99', '142.250.72.100', '204.44.192.60']
+    
     min_dist = inf
-    closest_server = replica_servers[0]
+    closest_server = REPLICA_SERVERS.keys()[0]
 
-    for server in replica_servers:
+    for server in REPLICA_SERVERS.keys():
         server_coordinates = find_location_coordinates(server)
         dist = distance_between_locations(server_coordinates, source_coordinates)
         if dist < min_dist:
@@ -77,17 +77,14 @@ def find_location_coordinates(ip):
     
     return (lat, lon)
 
-def resolve(client_address: Tuple[str, int])->str:
+def resolve(client_address: Tuple[str, int])-> Tuple[str, str]:
     client_ip = client_address[0]
     
-    if client_ip in CLIENT_REPLICA_ROUTING_TABLE:
-        return CLIENT_REPLICA_ROUTING_TABLE[client_ip]
-    
-    else:
+    if client_ip not in CLIENT_REPLICA_ROUTING_TABLE:
         closest_server = find_closest_replica_server(client_ip)
-        CLIENT_REPLICA_ROUTING_TABLE[client_ip] = closest_server
+        CLIENT_REPLICA_ROUTING_TABLE[client_ip] = {'replica_server' : closest_server, 'latency': inf}
 
-    return ORIGIN_IP
+    return (REPLICA_SERVERS[ CLIENT_REPLICA_ROUTING_TABLE[client_ip]['replica_server']], CLIENT_REPLICA_ROUTING_TABLE[client_ip]['replica_server'])
 
 def parse_args():
     parser = argparse.ArgumentParser(description='DNS Server')
@@ -115,32 +112,34 @@ def serve_dns(port, server_domain):
         message, address = udp_sock.recvfrom(bufferSize)
         query = DNSRecord.parse(message)
         logging.debug(f"Received {query} from {address}")
+        server_domain, server_ip = resolve(address)
         repsonse = DNSRecord(DNSHeader(id=query.header.id, qr=0, aa=1,
                                           ra=1),
                                 a=RR(server_domain,
-                                     rdata=A(resolve(address))))
+                                     rdata=A(server_ip)))
         logging.debug(f"Sending {repsonse} to {address}")
         udp_sock.sendto(repsonse.pack(), address)
 
-def ping(client_ip, replica_ip):
-    return requests.get('http://' + replica_ip + '/ping?client=' + client_ip)
-
-def ping_replicas(client_ip):
-    q = PriorityQueue()
-    for replica in REPLICA_SERVERS:
-        ping_time = ping(client_ip)
-        q.put((ping_time, replica))
-    
-    CLIENT_REPLICA_ROUTING_TABLE[client_ip] = q.get()[1]
-
 def update_client_replica_routing():
-    for client in CLIENT_REPLICA_ROUTING_TABLE:
-        ping_replicas(client)
-    
-    threading.Timer(300, update_client_replica_routing).start()
+    for replica in REPLICA_SERVERS.keys():
+        response = requests.get('http://' + replica + '/ping')
+        data = response.json()
+
+        for client in data:
+            if data[client] < CLIENT_REPLICA_ROUTING_TABLE[client]['latency']:
+                CLIENT_REPLICA_ROUTING_TABLE[client]['replica_server'] = replica
+                CLIENT_REPLICA_ROUTING_TABLE[client]['latency'] = data[client] 
+
+def ping_replica_servers():
+    for replica in REPLICA_SERVERS.keys():
+        body = {'clients' : list(CLIENT_REPLICA_ROUTING_TABLE.keys())}
+        requests.post('http://' + replica + '/ping', json = body)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     args = parse_args()
     update_client_replica_routing()
+    ping_replica_servers()
+    threading.Timer(UPDATE_ROUTING_TABLE_FREQUENCY, update_client_replica_routing).start()
+    threading.Timer(PING_REPLICA_SERVERS_FREQUENCY, ping_replica_servers).start()
     serve_dns(args.port, args.name)
