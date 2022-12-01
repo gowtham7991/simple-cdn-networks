d0db4ea98739d1d5f0e9ea58e380eec61086dcdf
decimalpack <decimalpack@users.noreply.github.com>
2022-11-30 23:13:20 -0500

    Update caching

--- httpserver.py
+++ httpserver.py
@@ -1,21 +1,30 @@
-import time
+import logging
 from aiohttp import web
 from aiohttp import ClientSession
-from functools import cache
+import brotli
+import py7zr
 
+logging.basicConfig(level=logging.DEBUG)
 app = web.Application()
 routes = web.RouteTableDef()
-cache = {}
+RAM_CACHE = {}
+DISK_CACHE = py7zr.SevenZipFile("disk.7z", mode="r")
 
 
 async def fetch_from_origin(path):
-    if path in cache:
-        return cache[path]
+    logging.debug(f"Fetching {path} from origin")
     async with ClientSession() as session:
         async with session.get(
                 f"http://cs5700cdnorigin.ccs.neu.edu:8080/{path}") as resp:
-            cache[path] = await resp.text()
-            return cache[path]
+            return await resp.text()
+
+
+@routes.post("/preload")
+async def preload(request):
+    for page in await request.json():
+        body = await fetch_from_origin(page)
+        RAM_CACHE[page] = brotli.compress(body.encode())
+    return web.Response(status=204)
 
 
 @routes.get("/grading/beacon")
@@ -25,9 +34,15 @@ async def beacon(request):
 
 @routes.get("/{path:.*}")
 async def proxy(request):
-    resp = await fetch_from_origin(request.match_info["path"])
-    time.sleep(5)
-    return web.Response(text=resp, content_type="text/html")
+    path = request.match_info["path"]
+    if path in RAM_CACHE:
+        response = brotli.decompress(RAM_CACHE[path]).decode()
+    elif path in DISK_CACHE.getnames():
+        response = DISK_CACHE.read(path)[path].read().decode()
+        DISK_CACHE.reset()
+    else:
+        response = await fetch_from_origin(path)
+    return web.Response(text=response, content_type="text/html")
 
 
 app.add_routes(routes)
