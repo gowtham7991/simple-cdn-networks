4cad11abe19163725679bbd9632cb78d0caee8a3
decimalpack <decimalpack@users.noreply.github.com>
2022-12-05 01:14:15 -0500

    Active measurement

--- dnsserver.py
+++ dnsserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+from collections import defaultdict
 from functools import lru_cache
 import logging
 import socket
@@ -13,6 +14,31 @@ import json
 from urllib.request import urlopen
 from math import radians, cos, sin, asin, sqrt, inf
 import geoip2.database
+
+ORIGIN = "cs5700cdnorigin.ccs.neu.edu"
+ORIGIN_IP = socket.gethostbyname(ORIGIN)
+
+CLIENTS = {
+    "35.175.146.183", "52.53.228.21", "18.192.115.31", "18.134.159.46",
+    "43.206.138.184"
+}
+CLIENT_REPLICA_ROUTING_TABLE = defaultdict(
+    lambda: defaultdict(lambda: {
+        "sum": 0,
+        "min": inf,
+        "max": -1,
+        "samples": 0
+    }))
+
+REPLICA_SERVERS = {
+    '139.144.30.25',
+    '173.255.210.124',
+    '139.144.69.56',
+    '185.3.95.25',
+    '139.162.83.107',
+    '192.46.211.228',
+    '170.187.240.5',
+}
 
 
 def get_ip_address():
@@ -28,21 +54,9 @@ def get_ip_address():
     return s.getsockname()[0]
 
 
-ORIGIN = "cs5700cdnorigin.ccs.neu.edu"
-ORIGIN_IP = socket.gethostbyname(ORIGIN)
-CLIENT_REPLICA_ROUTING_TABLE = {}
-REPLICA_SERVERS = {
-    '139.144.30.25',
-    '173.255.210.124',
-    '139.144.69.56',
-    '185.3.95.25',
-    '139.162.83.107',
-    '192.46.211.228',
-    '170.187.240.5',
-}
-
 UPDATE_ROUTING_TABLE_FREQUENCY = 300
 PING_REPLICA_SERVERS_FREQUENCY = 100
+
 
 def find_closest_replica_server(source_ip):
     DEFAULT_REPLICA_SERVER = '139.144.30.25'
@@ -76,7 +90,7 @@ def distance_between_locations(loc1, loc2):
     return (c * r)
 
 
-@lru_cache(maxsize=32)
+@lru_cache(maxsize=128)
 def find_location_coordinates(ip):
     # url = 'https://geolocation-db.com/jsonp/' + ip
     # response = urlopen(url)
@@ -92,15 +106,16 @@ def find_location_coordinates(ip):
 
 def resolve(client_address: Tuple[str, int]) -> str:
     client_ip = client_address[0]
+    CLIENTS.add(client_ip)
 
-    if client_ip not in CLIENT_REPLICA_ROUTING_TABLE:
-        closest_server = find_closest_replica_server(client_ip)
-        CLIENT_REPLICA_ROUTING_TABLE[client_ip] = {
-            'replica_server': closest_server,
-            'latency': inf
-        }
+    if client_ip in CLIENT_REPLICA_ROUTING_TABLE:
+        avg = lambda replica_ip: CLIENT_REPLICA_ROUTING_TABLE[client_ip][
+            replica_ip]["sum"] / CLIENT_REPLICA_ROUTING_TABLE[client_ip][
+                replica_ip]["samples"]
+        return min(CLIENT_REPLICA_ROUTING_TABLE[client_ip], key=avg)
+    else:
+        return find_closest_replica_server(client_ip)
 
-    return CLIENT_REPLICA_ROUTING_TABLE[client_ip]['replica_server']
 
 def parse_args():
     parser = argparse.ArgumentParser(description='DNS Server')
@@ -115,6 +130,7 @@ def parse_args():
                         help='CDN specific name')
     return parser.parse_args()
 
+
 def serve_dns(port):
 
     localIP = get_ip_address()
@@ -125,24 +141,42 @@ def serve_dns(port):
     logging.info(f"Listening on {localIP}:{port}")
     while True:
         message, address = udp_sock.recvfrom(bufferSize)
-        threading.Thread(target=process_request, args=(udp_sock, message, address)).start()
+        threading.Thread(target=process_request,
+                         args=(udp_sock, message, address)).start()
+
 
 def process_request(udp_sock, message, address):
     query = DNSRecord.parse(message)
     logging.debug(f"Received {query} from {address}")
     question = str(query.q).split()[0][1:-1]
     repsonse = DNSRecord(DNSHeader(id=query.header.id, qr=1, aa=0, ra=1),
-                             q=DNSQuestion(question),
-                             a=RR(question, rdata=A(resolve(address))))
+                         q=DNSQuestion(question),
+                         a=RR(question, rdata=A(resolve(address))))
     logging.debug(f"Sending {repsonse} to {address}")
     udp_sock.sendto(repsonse.pack(), address)
 
 
+def update_latency(output):
+    if output["type"] != "ping": return
+    client = output["dst"]
+    replica = output["src"]
+    latency = output["statistics"]["avg"]
+    CLIENT_REPLICA_ROUTING_TABLE[client][replica]["sum"] += latency
+    CLIENT_REPLICA_ROUTING_TABLE[client][replica]["min"] = min(
+        latency, CLIENT_REPLICA_ROUTING_TABLE[client][replica]["min"])
+    CLIENT_REPLICA_ROUTING_TABLE[client][replica]["max"] = max(
+        latency, CLIENT_REPLICA_ROUTING_TABLE[client][replica]["max"])
+    CLIENT_REPLICA_ROUTING_TABLE[client][replica]["samples"] += 1
+
+
 def ping_replica_servers():
     while True:
-        for replica in REPLICA_SERVERS.keys():
-            response = requests.post('http:/new_func/' + replica + ':25015/ping', json = " ".join(list(CLIENT_REPLICA_ROUTING_TABLE.keys())))
-            print(response.json())
+        for replica in REPLICA_SERVERS:
+            response = requests.post('http://' + replica + ':25015/ping',
+                                     data=" ".join(CLIENTS))
+            for output in response.text.split("\n"):
+                if output != "":
+                    update_latency(json.loads(output))
         time.sleep(PING_REPLICA_SERVERS_FREQUENCY)
 
 
